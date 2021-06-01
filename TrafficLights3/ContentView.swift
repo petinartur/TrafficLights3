607e47217eb8a26f4b3ad2a9dc4615850b8b2b9f
Artur <petin.art@gmail.com>
2021-06-01 13:50:16 +0300

    Initial Commit

---
+++ TrafficLights3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLights3
+//
+//  Created by Артур Петин on 01.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLights3/TrafficLights3App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLights3App.swift
+//  TrafficLights3
+//
+//  Created by Артур Петин on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLights3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
